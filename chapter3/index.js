fea730b7afab842141ccb6c46fc8d3d5c3d79366
nastyayasko <47418470+nastyayasko@users.noreply.github.com>
2020-01-09 12:15:04 +0300

    adds chapter 3 tasks

--- chapter3/index.js
+++ chapter3/index.js
@@ -3,11 +3,13 @@
 // 2
 
 const getMax = list => {
-  let max = list[0].value;
-  for (let i = 1; i < list.length; i++) {
-    if (list[i].value > max) {
-      max = list[i].value
-    }
+  let currentItem = list[0]
+  let max = currentItem.value;
+  while (currentItem.next != null) {
+    currentItem = list.find(({ value }) => value ===  currentItem.next)
+      if (currentItem.value > max) {
+        max = currentItem.value
+      }
   }
   return max;
 }
@@ -34,16 +36,16 @@ const addToBottom = (list, value) => {
 
 // 6
 const removeListItem = (list, itemValue) => {
-  let index;
+  let index = null;
   const item = list.find((listItem, i) => {
     if (listItem.value === itemValue) {
       index = i
       return listItem
     }
   })
-  list[index-1].next = item.next
+  list[index - 1].next = item.next
   if (item.next !== null) {
-    list[index+1].prev = item.prev
+    list[index + 1].prev = item.prev
   }
   list.splice(index, 1)
   return list
@@ -52,44 +54,96 @@ const removeListItem = (list, itemValue) => {
 // 8
 
 const insertValue = (list, value) => {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].value > value) {
-      list.splice(3, 0, { value })
-      break;
-    }
+  let currentItem = list[0]
+  let index = 0
+  if (value < currentItem.value) {
+    list.splice(index, 0, { value, prev: currentItem.prev, next: currentItem.value })
+    list.forEach(item => {
+      if (item.value === currentItem.value) {
+        item.prev = value
+      }
+    })
+    return
   }
+  while (value > currentItem.value) {
+    if (currentItem.next == null) {
+      list.forEach((item, i) => {
+        if (item.value === currentItem.value) {
+          item.next = value
+          index = i + 1
+        }
+      })
+      list.splice(index, 0, { value, prev: currentItem.value, next: null })
+      return
+    }
+    currentItem = list.find((listItem, i) => {
+      if (listItem.value === currentItem.next) {
+        index = i
+        return listItem
+      }
+    })
+  }
+  list.splice(index, 0, { value, prev: currentItem.prev, next: currentItem.value })
+  list.forEach(item => {
+    if (item.value === currentItem.value) {
+      item.prev = value
+    }
+  })
 }
 
 // 9
 const isSorted = list => {
-  for (let i = 1; i < list.length; i++) {
-    if (list[i].value < list[i-1].value) {
+  let currentItem = list[0]
+  while (currentItem.next != null) {
+    const nextItem = list.find(({ value }) => value ===  currentItem.next)
+    if (nextItem.value < currentItem.value) {
       return false
     }
+    currentItem = nextItem
   }
   return true
 }
 
 // 11
 
-class Planet {
-  constructor(name, distance, mass, diameter) {
-    this.name = name;
-    this.distance = distance;
-    this.mass = mass;
-    this.diameter = diameter;
-  }
-}
-
 const getPlanetList = (list, condition = 'distance') => {
+  const getMin = (list, condition, next)=> {
+    let currentItem = list[0]
+    let min = currentItem[condition];
+    while (currentItem[next] != null) {
+      currentItem = list.find(({ name }) => name ===  currentItem[next])
+      if (currentItem[condition] < min) {
+        min = currentItem.value
+      }
+    }
+    return list.find(item => item[condition] === min);
+  }
   if (condition === 'distance') {
-    return list.sort((a, b) => a.distance - b.distance)
+    let currentItem = getMin(list, condition, 'nextDistance')
+    const sortList = [currentItem]
+    while (currentItem.nextDistance != null) {
+      currentItem = list.find(({ name }) => name ===  currentItem.nextDistance)
+      sortList.push(currentItem)
+    }
+    return sortList
   }
   if (condition === 'mass') {
-    return list.sort((a, b) => a.mass - b.mass)
+    let currentItem = getMin(list, condition, 'nextMass')
+    const sortList = [currentItem]
+    while (currentItem.nextMass != null) {
+      currentItem = list.find(({ name }) => name ===  currentItem.nextMass)
+      sortList.push(currentItem)
+    }
+    return sortList
   }
   if (condition === 'diameter') {
-    return list.sort((a, b) => a.diameter - b.diameter)
+    let currentItem = getMin(list, condition, 'nextDiameter')
+    const sortList = [currentItem]
+    while (currentItem.nextDiameter != null) {
+      currentItem = list.find(({ name }) => name ===  currentItem.nextDiameter)
+      sortList.push(currentItem)
+    }
+    return sortList
   }
 }
 
--- chapter3/index.spec.js
+++ chapter3/index.spec.js
@@ -13,19 +13,19 @@ describe('Linked lists', () => {
   const list1 = [
     {
       value: 5,
-      next: 1,
-    },
-    {
-      value: 2,
       next: 2,
     },
     {
+      value: 2,
+      next: -1,
+    },
+    {
       value: -1,
-      next: 3,
+      next: 12,
     },
     {
       value: 12,
-      next: 4,
+      next: 0,
     },
     {
       value: 0,
@@ -35,19 +35,19 @@ describe('Linked lists', () => {
   const list2 = [
     {
       value: 1,
-      next: 1,
-    },
-    {
-      value: 2,
       next: 2,
     },
     {
-      value: 3,
+      value: 2,
       next: 3,
     },
     {
-      value: 4,
+      value: 3,
       next: 4,
+    },
+    {
+      value: 4,
+      next: 5,
     },
     {
       value: 5,
@@ -117,8 +117,42 @@ describe('Linked lists', () => {
     expect(newArr[1].value).toEqual(-1)
   })
   it('insert value in sorted list', () => {
-    insertValue(list1, 4)
-    expect(list1[3].value).toEqual(4)
+    const list = [
+      {
+        value: 1,
+        prev: null,
+        next: 2,
+      },
+      {
+        value: 2,
+        prev: 1,
+        next: 3,
+      },
+      {
+        value: 3,
+        prev: 2,
+        next: 5,
+      },
+      {
+        value: 5,
+        prev: 3,
+        next: 6,
+      },
+      {
+        value: 6,
+        prev: 5,
+        next: null,
+      },
+    ]
+    insertValue(list, 4)
+    expect(list[3].value).toEqual(4)
+    expect(list[4].prev).toEqual(4)
+    insertValue(list, 0)
+    expect(list[0].value).toEqual(0)
+    expect(list[1].prev).toEqual(0)
+    insertValue(list, 7)
+    expect(list[7].value).toEqual(7)
+    expect(list[6].next).toEqual(7)
   })
   it(`Should return true/false if list is/isn't sorted`, () => {
     expect(isSorted(list1)).toBeFalsy()
@@ -131,48 +165,72 @@ describe('Linked lists', () => {
         distance: 57900000,
         mass: 1,
         diameter: 4878,
+        nextDistance: 'Venus',
+        nextMass: 'Mars',
+        nextDiameter: 'Mars',
       },
       {
         name: 'Venus',
         distance: 108160000,
         mass: 3,
         diameter: 12104,
+        nextDistance: 'Earth',
+        nextMass: 'Earth',
+        nextDiameter: 'Earth',
       },
       {
         name: 'Earth',
         distance: 149600000,
         mass: 4,
         diameter: 12756,
+        nextDistance: 'Mars',
+        nextMass: 'Uranus',
+        nextDiameter: 'Neptune',
       },
       {
         name: 'Mars',
         distance: 227936640,
         mass: 2,
         diameter: 6794,
+        nextDistance: 'Jupiter',
+        nextMass: 'Venus',
+        nextDiameter: 'Venus',
       },
       {
         name: 'Jupiter',
         distance: 778369000,
         mass: 8,
         diameter: 142984,
+        nextDistance: 'Saturn',
+        nextMass: null,
+        nextDiameter: null,
       },
       {
         name: 'Saturn',
         distance: 1427034000,
         mass: 7,
         diameter: 120536,
+        nextDistance: 'Uranus',
+        nextMass: 'Jupiter',
+        nextDiameter: 'Jupiter',
       },
       {
         name: 'Uranus',
         distance: 2870658186,
         mass: 5,
         diameter: 51118,
+        nextDistance: 'Neptune',
+        nextMass: 'Neptune',
+        nextDiameter: 'Saturn',
       },
       {
         name: 'Neptune',
         distance: 4496976000,
         mass: 6,
         diameter: 49532,
+        nextDistance: null,
+        nextMass: 'Saturn',
+        nextDiameter: 'Uranus',
       },
     ]
 
