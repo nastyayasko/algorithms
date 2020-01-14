f0d05bb62045effbfac245164ea9685ee32805b8
nastyayasko <47418470+nastyayasko@users.noreply.github.com>
2020-01-14 17:05:17 +0300

    adds chapter 4 tasks

--- chapter4/index.js
+++ chapter4/index.js
@@ -38,10 +38,126 @@ const removeItem = (arr, index) => {
   return arr
 }
 
+// 5
+
+const topTriangle = size => {
+  const arr = []
+  for (let i = 0; i < size; i++) {
+    let arrRow = []
+    for (let j = 0; j < size; j++) {
+      if (j < i) {
+        arrRow.push(0)
+      } else {
+        arrRow.push(1)
+      }
+    }
+    arr.push(arrRow)
+  }
+  return arr
+}
+
+// 6
+
+const topLeftTriangle = size => {
+  const arr = []
+  for (let i = 0; i < size; i++) {
+    let arrRow = []
+    for (let j = 0; j < size; j++) {
+      if (size - 1 - j < i) {
+        arrRow.push(0)
+      } else {
+        arrRow.push(1)
+      }
+    }
+    arr.push(arrRow)
+  }
+  return arr
+}
+
+
+// 9
+
+const distanceArray = (row, col) => {
+  const arr = []
+  for (let i = 0; i < row; i++) {
+    let arrRow = []
+    for (let j = 0; j < col; j++) {
+      let distance = (j - 0)
+      if ((i - 0) < distance) {
+        distance = (i - 0)
+      }
+      if ((col - 1 - j) < distance) {
+        distance = (col - 1 - j)
+      }
+      if ((row - 1 - i) < distance) {
+        distance = (row - 1 - i)
+      }
+      arrRow.push(distance)
+    }
+    arr.push(arrRow)
+  }
+  return arr
+}
+
+// 11
+
+const topTriangleWithBreak = size => {
+  const arr = []
+  for (let i = 0; i < size; i++) {
+    let arrRow = []
+    for (let j = 0; j < size; j++) {
+      if (i < j) break
+      arrRow.push(1)
+    }
+    arr.push(arrRow)
+  }
+  return arr
+}
+
+// 12
+
+const sumOfTriangleArrays = (arr1, arr2) => {
+  if (arr1.length != arr2.length || arr1[0].length != arr2[0].length) return null
+  const arr = []
+  for (let i = 0; i < arr1.length; i++) {
+    let arrRow = []
+    for (let j = 0; j < arr2.length; j++) {
+      arrRow.push(arr1[i][j] + arr2[i][j])
+    }
+    arr.push(arrRow)
+  }
+  return arr
+}
+
+// 13
+
+const multiplyTriangleArrays = (arr1, arr2) => {
+  if (arr1.length != arr2.length || arr1[0].length != arr2[0].length) return null
+  const arr = []
+  for (let i = 0; i < arr1.length; i++) {
+    let arrRow = []
+    for (let j = 0; j < arr2.length; j++) {
+      let result = 0
+      for (let k = 0; k < arr2.length; k++) {
+        result = result + arr1[i][k] * arr2[k][j]
+      }
+      arrRow.push(result)
+    }
+    arr.push(arrRow)
+  }
+  return arr
+}
+
 
 module.exports = {
   getSampleVariance,
   getStandardDeviation,
   getMiddle,
   removeItem,
+  topTriangle,
+  topLeftTriangle,
+  distanceArray,
+  topTriangleWithBreak,
+  sumOfTriangleArrays,
+  multiplyTriangleArrays,
 }
